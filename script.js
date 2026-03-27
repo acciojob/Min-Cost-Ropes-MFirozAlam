726f4294c1e1d594fb939c161f154493f09e250d
MFirozAlam <146247719+MFirozAlam@users.noreply.github.com>
2026-03-27 23:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let cost = 0;
+
+	  while (arr.length > 1) {
+	    // sort array
+	    arr.sort((a, b) => a - b);
+	
+	    // take two smallest
+	    let first = arr.shift();
+	    let second = arr.shift();
+	
+	    let sum = first + second;
+	    cost += sum;
+	
+	    // push back combined rope
+	    arr.push(sum);
+  }
+
+  return cost;
   
 }
 
